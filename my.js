6746d82d50ff59d973155d23247d2e0513d1447e
mumtozBEK1234 <email manzilingiz>
2024-07-16 16:08:24 +0300

    1dars

---
+++ my.js
@@ -0,0 +1,93 @@
+// 1
+
+// let a = +prompt('son kiriting');
+// let natija = Math.floor(a / 2);
+// let qoldiq = a % 2;
+// console.log(natija);
+// console.log(qoldiq);
+
+// 2
+
+// let a = +prompt('son kiriting');
+// let b = +prompt('son kiriting');
+// if (a > b) {
+//     let kopaytma = a * b;
+//     console.log(kopaytma);
+// }else if (a == b) {
+// let ayirma = a - b;
+// console.log(ayirma);
+// }else {
+//     let yigindi = a + b;
+// console.log(yigindi);
+// }
+
+// 3
+
+// let a = +prompt('son kiritinng');
+// let result = 1;
+// for (let i = 2; i <= a ; i++ ){
+// result = result*i 
+// }
+// console.log(result);
+
+// 4
+
+// let a = +prompt('son kirit');
+// let b = +prompt('son kirit');
+// let c = +prompt('son kirit');
+// let resa = a * a ;
+// let resb = b * b ;
+// let resc = c * c ;
+// let result = resa + resb + resc;
+// console.log(result);
+
+// 5
+
+// let a = +prompt('son kiriting');
+// let b = +prompt('son kiriting');
+// let result = a**b;
+// console.log(result);
+
+// 6
+
+// let a = +prompt('son kiriting');
+// let ildiz = Math.sqrt(a);
+// let yaxlid = Math.round(ildiz);
+// console.log(ildiz);
+// console.log(yaxlid);
+
+// 7
+
+// let a = +prompt('son kiriting');
+// let b = +prompt('son kiriting');
+// res = 1;
+// result = 1;
+// for(let i = 1; i <= 5 ; i++){
+// res = res * a;
+// }
+// console.log(res);
+// for(let n = 1; n <= 5 ; n++){
+// result = result * b;
+// }
+// console.log(result);
+
+// 8
+
+// let a = +prompt('son kiriting');
+// let result = a * 10 - 7;
+// console.log(result);
+
+// 9
+
+// let a = +prompt('son kirit');
+// let result = a*a*a;
+// console.log(result);
+
+// 10
+
+// let a = +prompt('son kiritin');
+// let b = +prompt('son kiritin');
+// let c = +prompt('son kiritin');
+// let res = Math.pow(a*b*c,1/3);
+// let butun = Math.round(res)
+// console.log(butun);
